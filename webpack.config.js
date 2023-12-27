fd61e2fafc2419c3eab5c46e8713628ed5d07d42
유지원 <jna215843@gmail.com>
2023-12-27 23:00:54 +0900

    project setting ..

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader", // 바벨 로더를 추가한다
+      },
+    ],
+  },
+}
