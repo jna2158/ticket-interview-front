16faeb050107aa9195168e0b59ab5bed7f8fdc2f
유지원 <jna215843@gmail.com>
2023-12-28 21:22:46 +0900

    typescript 적용

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src', 'index.tsx'),
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
