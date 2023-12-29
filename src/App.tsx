9efc5ae492d6b29ddae8fd4cd8454ff26c4d51c2
유지원 <jna215843@gmail.com>
2023-12-29 14:31:33 +0900

    styled-component 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  width: 80px;
+  height: 50px;
+  background-color: gray;
+`;
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello, World!? </h1>
+        <Wrap>어서오세요.</Wrap>
       </div>
     );
   }
