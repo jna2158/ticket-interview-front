3e025f02f39c5e99765e88ff4a3cb4e240898ea8
유지원 <jna215843@gmail.com>
2024-01-14 14:52:33 +0900

    feat: naver state 값 수정

--- src/components/oauth/login/naverButton.tsx
+++ src/components/oauth/login/naverButton.tsx
@@ -4,7 +4,7 @@ import naverLogo from "../../../assets/image/btn_naver.svg";
 export default function NaverButton() {
   /** naver authorization server에 로그인 요청을 해서 authcode를 응답받는다. */
   const getAuthCode = () => {
-    const state = String(Math.floor(Math.random() * 100));
+    const state = Math.random().toString(36).substring(3, 14);
     const naverOAuthURI = `https://nid.naver.com/oauth2.0/authorize?client_id=${process.env.NAVER_OAUTH_CLIENT_ID}&redirect_uri=${process.env.NAVER_OAUTH_REDIRECT_URI}&response_type=code&state=${state}`;    
     window.location.href = naverOAuthURI;
   }
