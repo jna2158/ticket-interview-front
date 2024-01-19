8e57d947719e95809ce2f758bf494b83d57a7271
jiwon <jna2158@deepnoid.com>
2024-01-19 15:03:26 +0900

    feat: interview item animation

--- src/pages/InterviewSetting.tsx
+++ src/pages/InterviewSetting.tsx
@@ -1,125 +1,138 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useSpring, animated } from "react-spring";
 
 const calc = (x: number, y: number) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1];
 const trans = (x: number, y: number, s: number) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
+interface ISubject {
+  readonly id: string;
+  title: string;
+  checked: boolean;
+  problems: number;
+  left: string;
+};
+
+const [subjectArr, setSubjectArr] = useState<ISubject[]>([
+  {
+    id: "structure",
+    title: "1",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "algorithm",
+    title: "2",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "network",
+    title: "3",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "os",
+    title: "4",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "database",
+    title: "5",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "python",
+    title: "6",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "javascript",
+    title: "7",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "programming",
+    title: "8",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  },
+  {
+    id: "personal",
+    title: "9",
+    checked: false,
+    problems: 0,
+    left: "0%"
+  }
+]);
+
 export default function InterviewSetting() {
-  const [props, set] = useSpring(() => ([
-    { id: "structure", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "algorithm", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "network", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "os", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "database", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "python", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "javascript", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "programming", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } },
-    { id: "personal", xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }
-  ]));
-
-  interface Subject {
-    readonly id: string;
-    title: string;
-    checked: boolean;
-    problems: number;
-  };
-
-  const subjectArr: Subject[] = [
-    {
-      id: "structure",
-      title: "자료구조",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "algorithm",
-      title: "알고리즘",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "network",
-      title: "네트워크",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "os",
-      title: "운영체제",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "database",
-      title: "데이터베이스",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "python",
-      title: "python",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "javascript",
-      title: "javascript",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "programming",
-      title: "프로그래밍 공통",
-      checked: false,
-      problems: 0
-    },
-    {
-      id: "personal",
-      title: "인성",
-      checked: false,
-      problems: 0
-    }
-  ];
-
-  const handleCheckBox = (e: any) => {
-    // console.log(e.target.id);
-    // console.log(e.target.checked);
-  }
-  const handleCheckBoxClick = (e: any) => {
-    // set({ xys: calc(1200, 500) })
-    console.log("handleCheckBoxClick >> ");
-    console.log(e.target.id);
-    if (!e.target.id) return;
-
-    console.log("props");
-    console.log(props);
-    
-    
-  }
-
+  
   return (
     <SectionWrapper>
       <LeftSection>
-          {
-            subjectArr.map(el => {
-              return (
-                <animated.div
-                  onClick={(e: any) => handleCheckBoxClick(e)}
-                >
-                  <CheckBoxWrapper className="form-check">
-                    <CheckInput onChange={(e: any) => handleCheckBox(e)} className="form-check-input" type="checkbox" id={el.id} />
-                    <CheckLabel className="form-check-label border-bottom" htmlFor={el.id}>{el.title}</CheckLabel>
-                  </CheckBoxWrapper>
-                </animated.div>
-              )
-            })
-          }
+        {subjectArr.map((el: ISubject) => {
+          return (
+            <SubjectItem el={el} />
+          );
+        })}
       </LeftSection>
       <RightSection>오른쪽 영역</RightSection>
     </SectionWrapper>
-  )
+  );
 }
+
+const SubjectItem = ({el}: any) => {
+  
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 }
+  }));
+
+  /**
+   * 체크박스 클릭했을 때 애니메이션
+   * 
+   * @params e: event
+   */
+  const handleCheckBoxClick = (e: any) => {
+    if (!e.target) return;
+
+    e.target.checked ? set({ xys: calc(1200, 500)}) : set({ xys: [0, 0, 1] });
+
+  }
+
+  return (
+    <animated.div
+      onClick={(e: any) => handleCheckBoxClick(e)}
+      style={{ transform: props.xys.to(trans) }}
+    >
+      <CheckBoxWrapper left={el.left} className="form-check">
+        <CheckInput
+          className="form-check-input"
+          type="checkbox"
+          id={el.id}
+        />
+        <CheckLabel className="form-check-label border-bottom" htmlFor={el.id}>
+          {el.title}
+        </CheckLabel>
+      </CheckBoxWrapper>
+    </animated.div>
+  );
+}
+
 const SettingTitle = styled.div`
   font-size: 2rem;
   text-align: center;
@@ -144,6 +157,9 @@ const LeftSection = styled.div`
 const CheckBoxWrapper = styled.div`
   margin: 2%;
   height: 7.5vh;
+  left: ${(props) => {
+    return props.left;
+  }};
 `;
 
 const CheckInput = styled.input`
@@ -164,9 +180,12 @@ const CheckLabel = styled.label`
     transform: scale(1.05);
   }
 `;
+/* // LeftSection -- */
+
 
 /* Right Section */
 const RightSection = styled.div`
   width: 50%;
   height: auto;
 `;
+/* // RightSection -- */
