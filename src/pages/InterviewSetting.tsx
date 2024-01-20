907d7baabf6980d5971f594e739c7ef06653120f
유지원 <jna215843@gmail.com>
2024-01-20 15:18:30 +0900

    feat: 체크박스 체크 시 애니매이션 동작

--- src/pages/InterviewSetting.tsx
+++ src/pages/InterviewSetting.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useSpring, animated } from "react-spring";
+import * as _ from "lodash";
 
-const calc = (x: number, y: number) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1];
+const calc = (x: number, y: number) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.3];
 const trans = (x: number, y: number, s: number) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 interface ISubject {
@@ -12,93 +13,96 @@ interface ISubject {
   problems: number;
   left: string;
 };
-
-const subjectArr = [
-  {
-    id: "structure",
-    title: "자료구조",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "algorithm",
-    title: "알고리즘",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "network",
-    title: "네트워크",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "os",
-    title: "운영체제",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "database",
-    title: "데이터베이스",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "python",
-    title: "Python",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "javascript",
-    title: "Javascript",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "programming",
-    title: "프로그래밍 공통",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  },
-  {
-    id: "personal",
-    title: "인성",
-    checked: false,
-    problems: 0,
-    left: "0%"
-  }
-]
-
 export default function InterviewSetting() {
+  const [subjectArr, setSubjectArr] = useState([
+    {
+      id: "structure",
+      title: "자료구조",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "algorithm",
+      title: "알고리즘",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "network",
+      title: "네트워크",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "os",
+      title: "운영체제",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "database",
+      title: "데이터베이스",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "python",
+      title: "Python",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "javascript",
+      title: "Javascript",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "programming",
+      title: "프로그래밍 공통",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    },
+    {
+      id: "personal",
+      title: "인성",
+      checked: false,
+      problems: 0,
+      left: "0%"
+    }
+  ]);   
   
   return (
     <SectionWrapper>
       <LeftSection>
         {subjectArr.map((el: ISubject) => {
           return (
-            <SubjectItem el={el} />
+            <SubjectItem el={el} subjectArr={subjectArr} setSubjectArr={setSubjectArr}/>
           );
         })}
       </LeftSection>
-      <RightSection>오른쪽 영역</RightSection>
+      <RightSection></RightSection>
     </SectionWrapper>
   );
 }
 
-const SubjectItem = ({el}: any) => {
+const SubjectItem = ({el, subjectArr, setSubjectArr}: any) => {
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 }
+    config: { mass: 5, tension: 350, friction: 40 },
+    onRest: () => {
+      setTimeout(() => {
+        set({ xys: [0, 0, 1] });
+      }, 800);
+    }
   }));
 
   /**
@@ -107,10 +111,23 @@ const SubjectItem = ({el}: any) => {
    * @params e: event
    */
   const handleCheckBoxClick = (e: any) => {
-    if (!e.target) return;
+    if (!e.target || !e.target.id) return;
 
-    e.target.checked ? set({ xys: calc(1200, 500)}) : set({ xys: [0, 0, 1] });
+    // 애니메이션
+    e.target.checked ? set({ xys: calc(1000, 500)}) : set({ xys: [0, 0, 1] });
 
+    // 위치 이동
+    const clickedIndex = _.findIndex(subjectArr, {id: e.target.id});
+    const updatedSubjectArr = [...subjectArr];
+
+    if (clickedIndex !== -1) {
+      const left = subjectArr[clickedIndex].left === "0%" ? "110%" : "0%";
+      updatedSubjectArr[clickedIndex] = {
+        ...updatedSubjectArr[clickedIndex],
+        left: left,
+      };
+      setSubjectArr(updatedSubjectArr);
+    }
   }
 
   return (
@@ -156,6 +173,7 @@ const LeftSection = styled.div`
 const CheckBoxWrapper = styled.div<{box: string}>`
   margin: 2%;
   height: 7.5vh;
+  transition: left 2s, transform 2s;
   left: ${(props: any) => {
     return props.box;
   }};
