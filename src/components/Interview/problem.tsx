d39979ee59e8c5a4f9583c65919ffde95e335168
유지원 <jna215843@gmail.com>
2024-02-17 22:32:25 +0900

    feat: 문제 요청

---
+++ src/components/Interview/problem.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Problem() {
+  return (
+    <div className="col-md-8 contact-form">
+      <h3>Drop us a line</h3>
+      <form className="ajax-form" id="contactForm" method="post" action="assets/php/contact.php">
+          <div className="form-group">
+              <input type="text" className="form-control" id="name" name="name" placeholder="Your Name..." value="" required />
+          </div>
+          <div className="form-group">
+              <input type="email" className="form-control" id="email" name="email" placeholder="Your email..." value="" required />
+          </div>
+          <div className="form-group">
+              <input type="phone" className="form-control" id="phone" name="phone" placeholder="Your phone..." value="" required />
+          </div>
+          <div className="form-group">
+              <textarea className="form-control" name="message" placeholder="Your message..." required></textarea>
+          </div>
+          <div className="form-group">
+              <button type="submit" name="submit" className="btn btn-default"><i className="fa fa-paper-plane fa-fw"></i> Send</button>
+          </div>
+      </form>
+    </div>
+  )
+}
--- src/pages/ProblemSolve.tsx
+++ src/pages/ProblemSolve.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import Problem from "../components/Interview/problem";
 
 export default function ProblemSolve() {
   const { state: data } = useLocation();
   console.log("state", data);
   return (
-    <div>problem solve</div>
+    <Problem />
   )
 }
