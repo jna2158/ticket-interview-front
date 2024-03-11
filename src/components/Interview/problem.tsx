813c5cc19d3eb6eb3d991ee89c4c8ea9e4b198e8
유지원 <jna215843@gmail.com>
2024-03-11 23:12:12 +0900

    fix: 문제 풀기

--- src/components/Interview/problem.tsx
+++ src/components/Interview/problem.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { scoringProblem } from "../../services/InterviewService";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Problem({data, nextProblem, isLast}: any) {
   const [userAnswer, setUserAnswer] = useState("");
@@ -31,16 +32,56 @@ export default function Problem({data, nextProblem, isLast}: any) {
   }
 
   return (
-    <div className="col-md-8 contact-form">
-      <h3>{ data }</h3>
-      <form className="ajax-form" id="contactForm" onSubmit={e => e.preventDefault()}>
-          <div className="form-group">
-            <textarea className="form-control" name="message" placeholder="여기에 답을 입력하세요." value={userAnswer} onChange={e => setUserAnswer(e.target.value)}></textarea>
-          </div>
-          <div className="form-group">
-            <button className="btn btn-default" onClick={handleClickNextBtn}>{btnName} <i className="fa-solid fa-caret-right"></i></button>
-          </div>
-      </form>
-    </div>
+    // <div className="col-md-8 contact-form">
+    //   <h3>{ data }</h3>
+    //   <form className="ajax-form" id="contactForm" onSubmit={e => e.preventDefault()}>
+    //       <div className="form-group">
+    //         <textarea className="form-control" name="message" placeholder="여기에 답을 입력하세요." value={userAnswer} onChange={e => setUserAnswer(e.target.value)}></textarea>
+    //       </div>
+    //       <div className="form-group">
+    //         <button className="btn btn-default" onClick={handleClickNextBtn}>{btnName} <i className="fa-solid fa-caret-right"></i></button>
+    //       </div>
+    //   </form>
+    // </div>
+    <Wrapper>
+      <ProblemSection>
+        <span className="badge badge-warning">3/10</span>
+        <ProblemTitle>{ data }</ProblemTitle>
+          {/* <div className="form-group"> */}
+            <TextArea className="form-control" name="message" placeholder="여기에 답을 입력하세요." value={userAnswer} onChange={e => setUserAnswer(e.target.value)}></TextArea>
+          {/* </div> */}
+      </ProblemSection>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  background-color: #171717;
+  width: 100vw;
+  height: auto;
+  min-height: 100vh;
+`;
+const ProblemSection = styled.div`
+  width: 100vw;
+  padding: 5% 2% 2%;
+`;
+const ProblemTitle = styled.div`
+  font-size: 30px;
+  padding-bottom: 2%;
+  color: #E1E2E8;
+`;
+const TextArea = styled.textarea`
+  width: 65vw;
+  height: 50vh !important;
+  background-color: #1F2325;
+  border: 2px solid #495057;
+  color: #cbc8c8 !important;
+  line-height: 3vh;
+
+  &:hover {
+    background-color: #1F2325;
+  }
+  &:focus {
+    background-color: #1F2325;
+  }
+`;
