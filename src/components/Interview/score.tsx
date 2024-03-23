b3b547b3b1889bfcd705ef5c32ff950454d64819
유지원 <jna215843@gmail.com>
2024-03-23 13:36:35 +0900

    fix: 문제 결과 화면

--- src/components/Interview/score.tsx
+++ src/components/Interview/score.tsx
@@ -1,30 +1,69 @@
-import React from "react"
-import styled from "styled-components"
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import s_styled from "styled-components";
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
 
 export default function Score({ data, idx }: any) {
-  const textColor = data.score === 0 ? "text-danger" : ""
   return (
     <Wrapper>
-      <div className="accordion" id="scoreAccordion">
-        <div className="card">
-          <div className="card-header" id={idx}>
-            <h2 className="mb-0">
-              <button className={"btn btn-block text-left" + " " + textColor} type="button" data-toggle="collapse" data-target={"#collapseOne" + idx} aria-expanded="true" aria-controls={"collapseOne" + idx}>
-                { Object.keys(data)[0] }
-              </button>
-            </h2>
-          </div>
-          <div id={"collapseOne" + idx} className="collapse" aria-labelledby={idx} data-parent="#scoreAccordion">
-            <div className="card-body">
-              { data[Object.keys(data)[0]] }
-            </div>
-          </div>
-        </div>
-      </div>
+      <Accordion>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography style={{fontSize: '18px'}}>{Object.keys(data)[0]}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography style={{fontSize: '15px'}}>{data[Object.keys(data)[0]]}</Typography>
+        </AccordionDetails>
+      </Accordion>
     </Wrapper>
-  )
+  );
 }
 
-const Wrapper = styled.div`
+const Wrapper = s_styled.div`
   padding-bottom: 2vh;
 `;
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  borderRadius: '8px',
+  width: '100%',
+  height: 'fit-content',
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&::before': {
+    display: 'none',
+  }
+}));
+
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  backgroundColor: theme.palette.background.default,
+}));
