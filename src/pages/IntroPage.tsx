def398defae24fdd22ce9f25710f2b0e84b6bb1d
유지원 <jna215843@gmail.com>
2024-03-29 14:49:44 +0900

    feat: intro page background image

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import background from "../assets/images/cover-one.jpg";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { isLoginModalOpen } from "../redux/loginSlice";
+import ticketIntro from "../assets/image/ticket_intro.png";
+import ticketIntro2 from "../assets/image/ticket_intro2.png";
 
 export default function IntroPage() {
   const navigate = useNavigate();
@@ -19,40 +20,26 @@ export default function IntroPage() {
   };
 
   return (
-    // <header id="intro-carousel" className="carousel slide">
-    //   <div className="carorrusel-inner">
-    //     {/* <div className="item active"> */}
-    //     <BackgroundImage className="fill"></BackgroundImage>
-    //       <div className="carousel-caption">
-    //         <Section>Ticket</Section>
-    //         <Section>Video</Section>
-    //         <Section>Chatting</Section>
-    //         {/* <Title className="animated slideInDown">IT 면접에 자신감을 가지고 싶나요? <span className="highlight">이제는 준비할 때입니다.</span></Title>
-    //         <p className="intro-text animated slideInUp">다양한 카테고리별 예상 질문과 문제풀이로 당신을 돕습니다. Ticket Interview와 함께 더 나은 미래를 준비해보세요!</p>
-    //         <button className="btn btn-default btn-lg" onClick={startInterview}>면접 시작하기</button> */}
-    //       </div>
-    //       <div className="overlay-detail"></div>
-    //     {/* </div> */}
-    //   </div>
-    // </header>
     <Wrapper>
       <CategorySelectWrapper>
-        <TicketSection onClick={() => startInterview("Ticket")}>Ticket</TicketSection>
-        <VideoSection onClick={() => startInterview("Video")}>Video</VideoSection>
-        <ChatSection onClick={() => startInterview("Chatting")}>Chatting</ChatSection>
+        <TicketSection onClick={() => startInterview("Ticket")} image={ticketIntro} image2={ticketIntro2} />
+        {/* <VideoSection onClick={() => startInterview("Video")}>Video</VideoSection> */}
+        {/* <ChatSection onClick={() => startInterview("Chatting")}>Chatting</ChatSection> */}
       </CategorySelectWrapper>
+      <div className="carousel-caption">
+        <Title className="animated slideInDown">IT 면접에 자신감을 가지고 싶나요? <span className="highlight">이제는 준비할 때입니다.</span></Title>
+        <p className="intro-text animated slideInUp">다양한 카테고리별 예상 질문과 문제풀이로 당신을 돕습니다. TInterview와 함께 더 나은 미래를 준비해보세요!</p>
+      </div>
     </Wrapper>
   );
 }
 
-const BackgroundImage = styled.img`
-  background-color: #181818f6;
-  opacity: 0.85;
-`;
 const Title = styled.div`
   font-size: 4rem;
+  & span {
+    color: #fcdb59;
+  }
 `;
-
 const Wrapper = styled.div`
   background-color: #181818f6;
   height: 100vh;
@@ -61,14 +48,20 @@ const Wrapper = styled.div`
 const CategorySelectWrapper = styled.div`
   display: flex;
 `;
-const TicketSection = styled.section`
-  border: .1px solid white;
+const TicketSection = styled.section<{image, image2}>`
   height: 100vh;
   width: 33.3vw;
+  background-image: url(${props => props.image}),url(${props => props.image2});
+  background-size: contain;
+  background-position: 0px 55vh, 0px 65px;
+  background-repeat: no-repeat;
+  opacity: 0.4;
   &:hover {
-    background-color: red;
     transform: scale(1.02);
     transition: transform .5s;
+    opacity: 0.8;
+    transition: .4s ease-out;
+    cursor: pointer;
   }
 `;
 const VideoSection = styled.section`
@@ -91,8 +84,3 @@ const ChatSection = styled.section`
     transition: transform .5s;
   }
 `;
-
-
-// object-fit:cover;
-//   transform:scale(1.0);        
-//   transition: transform .5s; 
